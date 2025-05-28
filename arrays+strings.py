98935b982f3edc57ab01f17fcf49e25cf931215a
Nahom <8nahom8@gmail.com>
2025-05-28 13:44:41 -0500

    added recursion

--- arrays+strings.py
+++ arrays+strings.py
@@ -46,3 +46,9 @@ def reorder_integer(n):
 
 
 reorder_integer(34327)
+
+def factorial(n):
+    if n == 0:  # Base case: factorial of 0 is 1
+        return 1
+    else:
+        return n * factorial(n - 1)  # Recursive case
