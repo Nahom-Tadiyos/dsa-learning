55108d07b556063dbae1395f053ccbd0cf41f504
Nahom <8nahom8@gmail.com>
2025-06-02 11:05:58 -0500

    added traversing and coutning nodes

--- mooc/7.Trees_Recirson.py
+++ mooc/7.Trees_Recirson.py
@@ -12,3 +12,25 @@ node1 = Node(1, [node2, node3])
 
 node = Node(1)
 print(node)
+
+tree = Node(1, [Node(4, [Node(3), Node(7)]),
+                Node(5),
+                Node(2, [Node(6)])])
+
+#Traversing a tree - Recursion
+def traverse(node):
+    print("enter", node.value)
+    for child in node.children:
+        traverse(child)
+    print("leave", node.value)
+
+traverse(tree)
+
+#Computing information from a tree
+def count_nodes(node):
+    result = 1
+    for child in node.children:
+        result += count_nodes(child)
+    return result
+
+print(count_nodes(tree))
